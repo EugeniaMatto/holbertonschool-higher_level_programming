e7faf8ec643e7ec85f59b13f9b04f2b8d162a950
EugeniaMatto <eugematto26@gmail.com>
2022-04-25 11:40:10 -0700

    8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
+str = "{}{}{}".format(str[39:67], str[-22:-17], str[:6])
 print(str)
