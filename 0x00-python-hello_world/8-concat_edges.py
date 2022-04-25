e211ce02225105c56596e2380dc726aa4f4c8410
EugeniaMatto <eugematto26@gmail.com>
2022-04-25 11:31:27 -0700

    test

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "{}".format("object-oriented programming with Python")
+str = "object-oriented programming with Python"
 print(str)
