6e2f215e0008c892b4d4672130b1e3fb72d9f1be
EugeniaMatto <eugematto26@gmail.com>
2022-04-25 16:02:27 -0700

    100 fixed

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-import sys; sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n'); exit (1)
+import sys; sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n'); sys.exit(1)
