2451fe0ff8fec33c0a66c9b66d04fea000605c32
EugeniaMatto <eugematto26@gmail.com>
2022-04-26 12:05:58 -0700

    exe 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(101):
+        if (i % 3 == 0 or i % 5 == 0):
+            if i % 3 == 0:
+                print("Fizz", end=" ")
+            if i % 5 == 0:
+                print("Buzz", end=" ")
+        else:
+            print(f"{i:d}", end=" ")
