74d298094e1eaae534eb45995af04f5bf02b65ec
EugeniaMatto <eugematto26@gmail.com>
2022-04-26 12:07:35 -0700

    fixed exe 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(101):
+    for i in range(1, 101):
         if (i % 3 == 0 or i % 5 == 0):
             if i % 3 == 0:
                 print("Fizz", end=" ")
