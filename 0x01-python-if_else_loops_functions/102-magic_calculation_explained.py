2f37022c44626e3619557503422ba08ff36b39b3
EugeniaMatto <eugematto26@gmail.com>
2022-04-26 19:03:04 -0700

    exe 102

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation_explained.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if a < b:
+        return (c)
+    elif c > b:
+        return (a + b)
+    return (a * b - c)
+
+
+import dis
+dis.dis(magic_calculation)
