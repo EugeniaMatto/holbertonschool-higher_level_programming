c060912382204ed128b9279eef48d402259739de
EugeniaMatto <eugematto26@gmail.com>
2022-04-28 08:32:33 -0700

    exe 1 fixed pep8

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	from calculator_1 import add, div, mul, sub
-	a = 10
-	b = 5
-	print(f"{a:d} + {b:d} = {add(a, b):d}")
-	print(f"{a:d} - {b:d} = {sub(a, b):d}")
-	print(f"{a:d} * {b:d} = {mul(a, b):d}")
-	print(f"{a:d} / {b:d} = {div(a, b):d}")
+    from calculator_1 import add, div, mul, sub
+    a = 10
+    b = 5
+    print(f"{a:d} + {b:d} = {add(a, b):d}")
+    print(f"{a:d} - {b:d} = {sub(a, b):d}")
+    print(f"{a:d} * {b:d} = {mul(a, b):d}")
+    print(f"{a:d} / {b:d} = {div(a, b):d}")
