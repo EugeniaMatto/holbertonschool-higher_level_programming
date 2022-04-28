db1ec9e3b7b697d60c41bbdd1bbde256a85cf5cd
EugeniaMatto <eugematto26@gmail.com>
2022-04-28 12:13:15 -0700

    exe 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     le = len(sys.argv)
     if (le != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
     if (sys.argv[2] != '+' and sys.argv[2] != '/' and
             sys.argv[2] != '*' and sys.argv[2] != '-'):
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
     if (sys.argv[2] == '+'):
         cuenta = int(sys.argv[1]) + int(sys.argv[3])
     if (sys.argv[2] == '-'):
