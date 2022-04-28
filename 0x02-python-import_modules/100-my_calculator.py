9e2626be7b10a8da2fcc42c248892846197cb9a0
EugeniaMatto <eugematto26@gmail.com>
2022-04-28 11:38:49 -0700

    exe 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     if (le != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if (sys.argv[2] != '+' and sys.argv[2] != '/' and sys.argv[2] != '*' and sys.argv[2] != '-'):
+    if (sys.argv[2] != '+' and sys.argv[2] != '/' and
+            sys.argv[2] != '*' and sys.argv[2] != '-'):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     if (sys.argv[2] == '+'):
@@ -14,7 +15,7 @@ if __name__ == "__main__":
     if (sys.argv[2] == '-'):
         cuenta = int(sys.argv[1]) - int(sys.argv[3])
     if (sys.argv[2] == '*'):
-        cuenta = int(sys.argv[1]) * int(sys.argv[3]) 
+        cuenta = int(sys.argv[1]) * int(sys.argv[3])
     if (sys.argv[2] == '/'):
         cuenta = int(sys.argv[1]) / int(sys.argv[3])
     print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {cuenta}")
