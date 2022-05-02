70767809798760801ee6a130ab00388e68feb955
EugeniaMatto <eugematto26@gmail.com>
2022-05-02 15:03:35 -0700

    ex 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    new = []
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            new[i] = True
-        else:
-            new[i] = False
-    return new
+def divisible_by_2(my_list=[]):
+    return [(elem % 2 == 0) for elem in my_list]
