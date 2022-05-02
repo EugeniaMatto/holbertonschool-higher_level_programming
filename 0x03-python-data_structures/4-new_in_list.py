920a305bdeec24355d4a4da6afd13493ea51f54b
EugeniaMatto <eugematto26@gmail.com>
2022-05-02 11:32:16 -0700

    ex 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if (my_list == None or idx < 0 or idx > (len(my_list) - 1)):
+        return my_list
+    b = my_list.copy()
+    b[idx] = element
+    return b
