94bf76bb214499e48b95f7e306d47c8bded0f01b
EugeniaMatto <eugematto26@gmail.com>
2022-05-02 10:43:50 -0700

    ex 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if (idx > (len(my_list) - 1) or idx < 0):
+        return my_list
+    my_list[idx] = element
+    return my_list
