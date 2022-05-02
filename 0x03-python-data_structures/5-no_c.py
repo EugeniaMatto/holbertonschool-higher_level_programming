67918025c60fec4ec4011c1b737128052989e576
EugeniaMatto <eugematto26@gmail.com>
2022-05-02 11:58:55 -0700

    ex 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    if my_string is None:
+        return my_string
     for i in range(len(my_string)):
         if (my_string[i:i+1] == 'C') or (my_string[i:i+1] == 'c'):
             my_string = my_string[:i] + my_string[i + 1:]
-            i = 0
     return my_string
