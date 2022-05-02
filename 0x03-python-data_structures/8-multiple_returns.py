011530de4cd7d10375dec2fff12c3a447e82018a
EugeniaMatto <eugematto26@gmail.com>
2022-05-02 14:29:52 -0700

    ex 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return None, 0
+    if sentence == "":
+        return 0, None
     return len(sentence), sentence[0]
