d8ed1bbf3be86359bcc1a4958e3efc968c3e6913
EugeniaMatto <eugematto26@gmail.com>
2022-05-02 12:34:37 -0700

    ex 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix[0])):
         for j in range(len(matrix)):
-            print(f"{matrix[i][j]}", end=" ")
-        print()
+            print(f"{matrix[i][j]}", end="")
+            if (j != len(matrix) - 1):
+                print(" ", end="")
+        if (i != len(matrix[0]) - 1):
+            print()
+    print()
