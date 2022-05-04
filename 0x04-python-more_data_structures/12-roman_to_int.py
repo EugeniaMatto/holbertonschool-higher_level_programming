3237e858bc15a6eba4c23569f336761ff0214db4
EugeniaMatto <eugematto26@gmail.com>
2022-05-04 14:45:12 -0700

    ex 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,25 +2,26 @@
 def roman_to_int(roman_string):
     suma = 0
     if (isinstance(roman_string, str) and roman_string):
-        for i in range(len(roman_string)):
+        roman_string += "_"
+        for i in range(len(roman_string) - 1):
             if roman_string[i] == 'I':
-                if roman_string[i - 1] == 'V' or roman_string[i - 1] == 'X':
+                if roman_string[i + 1] == 'V' or roman_string[i + 1] == 'X':
                     suma -= 1
                 else:
                     suma += 1
             if roman_string[i] == 'V':
                 suma += 5
             if roman_string[i] == 'X':
-                suma += 10
-            if roman_string[i] == 'L':
-                if roman_string[i - 1] == 'L' or roman_string[i - 1] == 'C':
-                    suma -= 50
+                if roman_string[i + 1] == 'L' or roman_string[i + 1] == 'C':
+                    suma -= 10
                 else:
+                    suma += 10
+            if roman_string[i] == 'L':
                     suma += 50
             if roman_string[i] == 'C':
                 suma += 100
             if roman_string[i] == 'D':
-                if roman_string[i - 1] == 'M':
+                if roman_string[i + 1] == 'M':
                     suma -= 500
                 else:
                     suma += 500
