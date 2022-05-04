a6a1b2094b8e8f0ad9880a2588eb0771cc619842
EugeniaMatto <eugematto26@gmail.com>
2022-05-04 14:01:00 -0700

    ex 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    suma = 0
+    if (isinstance(roman_string, str) && roman_string):
+        for i in roman_string:
+            if i == 'I':
+                suma += 1
+            if i == 'V':
+                suma += 5
+            if i == 'X':
+                suma += 10
+            if i == 'L':
+                suma += 50
+            if i == 'C':
+                suma += 100
+            if i == 'D':
+                suma += 500
+            if i == 'M':
+                suma += 1000
+    return suma
