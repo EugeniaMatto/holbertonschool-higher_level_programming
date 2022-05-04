ce991cd7c867ff0d52a6f9f9ae4295908ebe4fc9
EugeniaMatto <eugematto26@gmail.com>
2022-05-04 13:02:42 -0700

    ex 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    s1 = set_1.difference_update(set_2)
-    s2 = set_2.difference_update(set_1)
-    return s1.update(s2)
+    s = set_1.symmetric_difference_update(set_2)
+    return s
