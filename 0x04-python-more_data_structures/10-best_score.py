dfaac043e68995d39b256c4273fa3c75ed1823ef
EugeniaMatto <eugematto26@gmail.com>
2022-05-04 13:52:21 -0700

    ex 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,9 @@
 def best_score(a_dictionary):
     max = -255
     nombre = None
-    for i in list(a_dictionary):
-        if (a_dictionary[i] > max):
-            max = a_dictionary[i]
-            nombre = i
+    if a_dictionary:
+        for i in list(a_dictionary):
+            if (a_dictionary[i] > max):
+                max = a_dictionary[i]
+                nombre = i
     return nombre
