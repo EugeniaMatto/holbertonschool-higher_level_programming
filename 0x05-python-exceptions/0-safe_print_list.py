1e5587d998e38a9fc9fa8eca7422ee52b9f2c009
EugeniaMatto <eugematto26@gmail.com>
2022-05-16 11:54:12 -0700

    ex 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x): print(my_list[i], end="" if i != x - 1 else "\n")
+        return x
+    except IndexError:
+        print()
+        return i
