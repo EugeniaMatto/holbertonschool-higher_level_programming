9c479ac81011152860933457fb78d5067a3f6db0
EugeniaMatto <eugematto26@gmail.com>
2022-05-16 12:47:38 -0700

    ex 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             elems += 1
         except (TypeError, ValueError):
             pass
-        except IndexError:
-            break
     print()
     return elems
