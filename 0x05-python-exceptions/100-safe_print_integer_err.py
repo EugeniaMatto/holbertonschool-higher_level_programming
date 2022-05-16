77732ba0163cc90ac64e581bc7d8c6de479d26b6
EugeniaMatto <eugematto26@gmail.com>
2022-05-16 14:33:27 -0700

    ex 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as te:
+        print("Exception: {}".format(te))
+        return False
