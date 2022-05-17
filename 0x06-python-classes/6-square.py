fec4ef8d7b4b42fb8b09f11b4b78f4f940858f06
EugeniaMatto <eugematto26@gmail.com>
2022-05-17 12:44:23 -0700

    ex 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Define Square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ Init Square """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Size Square"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """getter position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter position"""
+        if type(value) != tuple or type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """ Area Square """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """ Print Square """
+        if (self.__size > 0):
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end="")
+                for k in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
