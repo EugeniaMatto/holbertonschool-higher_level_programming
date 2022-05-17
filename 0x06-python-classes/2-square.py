0f584e424cd3aeed532aaa1db422f831bbf865d5
EugeniaMatto <eugematto26@gmail.com>
2022-05-17 12:02:13 -0700

    fixed comments

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""Class Square"""
+
 class Square:
+    """Define Square"""
 
     def __init__(self, size=0):
+        """Init Class"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
