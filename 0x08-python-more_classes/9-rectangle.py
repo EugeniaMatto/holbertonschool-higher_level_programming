6e8e0feacf2aebd329c1b66bf27b79c6eb96a981
EugeniaMatto <eugematto26@gmail.com>
2022-05-18 13:40:26 -0700

    fixed ex 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -85,4 +85,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """ returns a new Rectangle instance with width == height == size """
-        return Rectangle(size, size)
+        return cls(size, size)
