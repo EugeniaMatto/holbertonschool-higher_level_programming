63a3902ae63be5fd2c38b2738548ecff586ed829
EugeniaMatto <eugematto26@gmail.com>
2022-05-18 14:21:36 -0700

    removed comments

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -19,19 +19,8 @@ if n < 4:
     print("N must be at least 4")
     sys.exit(1)
 
-""" solutions: n - 2 (all positions but borders) """
-nums = []
-for i in range(n + 1):
-    nums.append(i)
-
-""" second position always start in 3 y va sumando de a 2
-    en horizontal la segunda posicion"""
 start = 3
-""" second position increments this way: {2, 3, 4, ..., N} in vertical
-    in bucle hasta N(ex. N = 6,
-    si incremento de a 2 y soy 5 entonces quedo 0)"""
 i = 2
-""" solution number """
 solN = 0
 sol = []
 while(solN < n - 2):
@@ -40,7 +29,6 @@ while(solN < n - 2):
         sol[solN].append([j, '-'])
     sol[solN][0][1] = solN + 1
     solN += 1
-""" sol[solN][pos][[i],[j]] """
 pos = 1
 while(pos < n):
     sc = start
