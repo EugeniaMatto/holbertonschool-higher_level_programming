b509b13f861edcd9836583b27be5d8345bb36b72
EugeniaMatto <eugematto26@gmail.com>
2022-05-18 14:05:44 -0700

    fixed ex 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,20 +4,16 @@
 
 class Rectangle:
     """ ···Define Rectangle··· """
-
     def __init__(self, width=0, height=0):
-        """ init function """
         self.height = height
         self.width = width
 
     @property
     def width(self):
-        """ get width """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ setter width """
         if (type(value) != int):
             raise TypeError("width must be an integer")
         if (value < 0):
@@ -26,12 +22,10 @@ class Rectangle:
 
     @property
     def height(self):
-        """ get height """
         return sef.__self
 
     @height.setter
     def height(self, value):
-        """ setter height """
         if (type(value) != int):
             raise TypeError("height must be an integer")
         if (value < 0):
