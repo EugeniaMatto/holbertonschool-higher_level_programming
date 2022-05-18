e9b91353d30c61331957e703fbf0610b995d5295
EugeniaMatto <eugematto26@gmail.com>
2022-05-18 14:13:22 -0700

    fixed ex 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ init class """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
