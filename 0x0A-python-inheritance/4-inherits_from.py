41f07370cef00b4d352296d69c9494030b7154c7
EugeniaMatto <eugematto26@gmail.com>
2022-05-30 15:16:24 -0700

    ex 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """ inherits_from """
-    return isinstance(ob, a_class) and (type(obj) != a_class)
+    return isinstance(obj, a_class) and (type(obj) != a_class)
