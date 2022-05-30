4bde542284722ba9676a1aca6efa91debe3d0d98
EugeniaMatto <eugematto26@gmail.com>
2022-05-30 14:06:26 -0700

    ex 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Ex. 1 """
+
+
+class MyList(list):
+    """ class MyList """
+    pass
+
+    def print_sorted(self):
+        new = self.copy();
+        for i in range(len(new) - 1):
+            if new[i] > new[i + 1]:
+                aux = new[i]
+                new[i] = new[i + 1]
+                new[i + 1] = aux
+        print(new)
