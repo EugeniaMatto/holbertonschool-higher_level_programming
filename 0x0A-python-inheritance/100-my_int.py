8d0a330473c29b5e6cdf21400e91311dd5c3ecf3
EugeniaMatto <eugematto26@gmail.com>
2022-05-30 18:53:52 -0700

    ex 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+""" ex 12 """
+
+
+class MyInt(int):
+    """ My int """
+
+    def __init__(self, n):
+        """ init """
+        self.__n = n
+
+    def __eq__(self, other):
+        """ override eq """
+        return self.__n != other
+
+    def __ne__(self, other):
+        """ override ne """
+        return self.__n == other
