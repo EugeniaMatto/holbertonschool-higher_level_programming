6ede8beed74ecfc2db4363856ef442f15a6d3cf8
EugeniaMatto <eugematto26@gmail.com>
2022-05-30 19:03:52 -0700

    ex 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """ ex 12 """
 
 
@@ -7,8 +7,8 @@ class MyInt(int):
 
     def __eq__(self, other):
         """ override eq """
-        return super().__ne__(other)
+        return (super().__ne__(other))
 
     def __ne__(self, other):
         """ override ne """
-        return super().__eq__(other)
+        return (super().__eq__(other))
