b89dff867135dae451da50c66336a77a7de5d713
EugeniaMatto <eugematto26@gmail.com>
2022-05-30 19:19:17 -0700

    ex 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ ex 13 """
 
+
 def add_attribute(obj, attribute, value):
+    """ add attribute """
     if ("__dict__" is not dir(obj)):
         raise TypeError("can't add new attribute")
     setattr(obj, attribute, value)
