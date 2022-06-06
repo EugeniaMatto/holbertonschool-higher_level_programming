efaf643aeef2e8713c1b8ffb65218aa7faf4f79f
EugeniaMatto <eugematto26@gmail.com>
2022-06-06 15:51:25 -0700

    ex 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,7 @@ class Rectangle(Base):
         """ prints in stdout the rectangle instance with #"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """ override str """
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
