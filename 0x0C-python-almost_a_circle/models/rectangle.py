3a665dca5c4eb1589b674294d607a65ea05fc284
EugeniaMatto <eugematto26@gmail.com>
2022-06-06 15:57:08 -0700

    ex 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,10 +77,10 @@ class Rectangle(Base):
 
     def display(self):
         """ prints in stdout the rectangle instance with #"""
-        for i in range(self.__x):
+        for i in range(self.__y):
             print()
         for i in range(self.__height):
-            print((" " * self.__y) + ("#" * self.__width))
+            print((" " * self.__x) + ("#" * self.__width))
 
     def __str__(self):
         """ override str """
