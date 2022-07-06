778003d1722c446627dbacbad9a40d75acaf9c95
EugeniaMatto <eugematto26@gmail.com>
2022-07-05 18:06:38 -0700

    ex 15

--- 0x0D-SQL_introduction/15-groups.sql15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql15-groups.sql
@@ -1,2 +1,2 @@
 -- SQL
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY score DESC
