aa4258a516a0457488455654b1d8f3466b541024
EugeniaMatto <eugematto26@gmail.com>
2022-07-05 18:16:29 -0700

    ex 102

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- SQL
-SELECT city, AVG(value) AS avg_temp FROM hbtn_0c_0.temperatures GROUP BY city ORDER BY avg_temp DESC
+SELECT city, AVG(value) AS avg_temp FROM hbtn_0c_0.temperatures GROUP BY city ORDER BY AVG(value) DESC;
