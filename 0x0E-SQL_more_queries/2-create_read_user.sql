03466ebc0de2ce4cf25c3933e7cd3d5a54ccdeac
EugeniaMatto <eugematto26@gmail.com>
2022-07-06 18:42:42 -0700

    ex 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- command to create user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- command to give privilege SELECT to user
-GRANT SELECT PRIVILEGE ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
