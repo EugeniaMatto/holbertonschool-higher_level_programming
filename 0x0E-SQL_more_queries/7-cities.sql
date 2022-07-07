6ef31ee3f3581e5422109a976a324e44c1a595e7
EugeniaMatto <eugematto26@gmail.com>
2022-07-06 20:05:48 -0700

    ex 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,4 +3,4 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE
-	, FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id), name VARCHAR(256) NOT NULL)
+	, state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id), name VARCHAR(256) NOT NULL)
