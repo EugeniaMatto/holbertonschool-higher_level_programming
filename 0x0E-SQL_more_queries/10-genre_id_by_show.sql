e10c9caacb513af89f4977492e50a07eabbdb0da
EugeniaMatto <eugematto26@gmail.com>
2022-07-07 11:07:05 -0700

    ex 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- SQL
 SELECT sh.title, sh_ge.genre_id
-FROM tv_shows sh INNER JOIN tv_shows_genres sh_ge
+FROM tv_shows as sh INNER JOIN tv_show_genres as sh_ge
 ON
 	sh.id = sh_ge.show_id
-ORDER BY sh.title, sh_ge.genre_id ASC
+ORDER BY sh.title, sh_ge.genre_id;
