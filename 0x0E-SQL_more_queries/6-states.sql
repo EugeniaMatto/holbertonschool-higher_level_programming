65b56d46ef9245d3018ae27fa71a008642373dc0
EugeniaMatto <eugematto26@gmail.com>
2022-07-06 19:59:36 -0700

    ex 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- script that creates the database hbtn_0d_usa and the table states
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE states(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL)
