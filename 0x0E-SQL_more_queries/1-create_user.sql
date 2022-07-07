a4d147b593ae375933895269f7bfe524203c6753
EugeniaMatto <eugematto26@gmail.com>
2022-07-06 17:51:17 -0700

    ex 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,6 +3,6 @@ CREATE USER
 	IF NOT EXISTS 'user_0d_1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-	ON .
+	ON *.*
 	TO 'user_0d_1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
