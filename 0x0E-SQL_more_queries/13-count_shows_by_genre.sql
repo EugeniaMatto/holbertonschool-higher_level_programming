e31fb00df60577c06ade5425557a0827735008e2
EugeniaMatto <eugematto26@gmail.com>
2022-07-07 11:42:32 -0700

    ex 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- SQL
 SELECT ge.name as genre, COUNT(sh_ge.genre_id) as number_of_shows
-FROM tv_genres ge JOIN tv_show_genres sh_ge
+FROM tv_genres ge INNER JOIN tv_show_genres sh_ge
 ON
 	ge.id = sh_ge.genre_id
 GROUP BY ge.name
