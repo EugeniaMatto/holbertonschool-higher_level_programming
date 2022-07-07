f36bc592215bdae218534cafbdd68463649c4d49
EugeniaMatto <eugematto26@gmail.com>
2022-07-07 08:58:10 -0700

    ex9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,7 @@
 -- script that lists all the cities of California in database hbtn_0d_usa
 
-SELECT c.id, c.name, s.name
+SELECT DISTINCT c.id, c.name, s.name
 FROM cities c, states s
 WHERE
-	s.name = 'California' AND
 	c.state_id = s.id
 ORDER BY c.id ASC
