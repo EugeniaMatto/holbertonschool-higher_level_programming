de0b91d603d8ba494a614b98b09f8245523b2906
EugeniaMatto <eugematto26@gmail.com>
2022-07-06 17:49:42 -0700

    ex 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- script that creates the MySQL server user
+CREATE USER
+	IF NOT EXISTS 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON .
+	TO 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
