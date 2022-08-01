8539a780747b74ebcbed807d905300b515579b04
EugeniaMatto <eugematto26@gmail.com>
2022-08-01 15:32:05 -0700

    ex 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 /* script that prints JavaScript is amazing */
-const myVar = 'C is Fun \nPython is cool \nJavaScript is amazing';
+const myVar = 'C is fun \nPython is cool \nJavaScript is amazing';
 console.log(myVar);
