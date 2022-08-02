e702b88996ecc5bc3f05fd008b83f31ecc594136
EugeniaMatto <eugematto26@gmail.com>
2022-08-01 17:50:02 -0700

    ex 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,11 @@
 #!/usr/bin/node
 /* script */
 const argv = process.argv;
-const myVar = [];
 if (!argv[2] || !argv[3]) {
   console.log(0);
 } else {
-  let i = 0;
-
-  for (const elem of argv) {
-    i++;
-    if (i > 2) {
-      myVar.push(parseInt(elem));
-    }
-  }
-  myVar.sort();
-  console.log(myVar[myVar.length - 2]);
+  argv.shift();
+  argv.shift();
+  argv.sort();
+  console.log(argv[argv.length - 2]);
 }
