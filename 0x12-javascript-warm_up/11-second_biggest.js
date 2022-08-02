e87ec8c96952ad970f40a99417fd699a3be1c3e2
EugeniaMatto <eugematto26@gmail.com>
2022-08-01 19:08:53 -0700

    ex 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,9 @@
 /* script */
 const argv = process.argv.slice(2);
 argv.sort();
+argv.reverse();
 if (argv.length < 2) {
   console.log(0);
 } else {
-  console.log(argv[argv.length - 2]);
+  console.log(argv[1]);
 }
