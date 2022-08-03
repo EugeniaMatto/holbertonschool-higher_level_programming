4a7282a99bf9d96111aee892be830dc04e5d64ae
EugeniaMatto <eugematto26@gmail.com>
2022-08-03 08:38:41 -0700

    ex 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return (list.reverse());
+  const i = [];
+  for (let j = list.length; j > 0; j--) {
+    i.push(list[j - 1]);
+  }
+  return (i);
 };
