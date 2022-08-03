e21d0bfbbd2bd33484a2dcd9a89b09e389e4783a
EugeniaMatto <eugematto26@gmail.com>
2022-08-03 08:26:08 -0700

    ex 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
