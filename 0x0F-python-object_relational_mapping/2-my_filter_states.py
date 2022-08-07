c16567ac34aae9b039e7d56f1bd904d646c6aa1c
EugeniaMatto <eugematto26@gmail.com>
2022-08-07 16:33:03 -0700

    ex 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
             host="localhost", port=3306,
             user=av[1], passwd=av[2], db=av[3])
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM states WHERE name LIKE '{av[4]}' \
-                ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' \
+                ORDER BY id ASC".format(av[4]))
     rows = cur.fetchall()
     for row in rows:
         if row[1][0] == 'N':
