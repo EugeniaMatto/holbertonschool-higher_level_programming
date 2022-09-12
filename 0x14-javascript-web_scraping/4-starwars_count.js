bf0fdd824c64cba73db49677eb8855f80f46968a
EugeniaMatto <eugematto26@gmail.com>
2022-09-12 22:46:55 +0000

    task 4 fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,16 @@
 
 const axios = require('axios').default;
 const url = process.argv[2];
-const pj = 'https://swapi-api.hbtn.io/api/people/18/';
+const pj = '18';
 
 axios.get(url)
   .then(function (response) {
     const results = response.data.results;
     let pelis = 0;
     results.forEach(r => {
-      if (r.characters.includes(pj)) { pelis++; }
+      (r.characters).forEach(c => {
+        if (c.includes(pj)) { pelis++; }
+      });
     });
     console.log(pelis);
   });
