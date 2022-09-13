18a0ba918554b55a02e4ee94d58c96afffe198e2
EugeniaMatto <eugematto26@gmail.com>
2022-09-13 03:06:16 +0000

    task 6 fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,31 +2,18 @@
 
 const axios = require('axios').default;
 const url = process.argv[2];
-dict = {
-  1: 0,
-  2: 0,
-  3: 0,
-  4: 0,
-  5: 0,
-  6: 0,
-  7: 0,
-  8: 0,
-  9: 0,
-  10: 0
-};
 
 axios.get(url)
   .then(function (response) {
     const all = response.data;
+    const dict = {};
     all.forEach(function (e) {
       if (e.completed) {
         let ch = String.fromCharCode(48 + e.userId);
-        if (ch == ':') { ch = '10'; }
+        if (ch === ':') { ch = '10'; }
+        if (!(Object.keys(dict).includes(ch))) { dict[ch] = 0; }
         dict[ch] = dict[ch] + 1;
       }
     });
-    for (const [k, v] of Object.entries(dict)) {
-      if (v == 0) { delete dict.k; }
-    }
     console.log(dict);
   });
