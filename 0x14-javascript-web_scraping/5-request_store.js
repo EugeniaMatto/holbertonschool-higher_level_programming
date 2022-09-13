9a535e1073a54ac7a2ef6206aa2554ce7f0af3e0
EugeniaMatto <eugematto26@gmail.com>
2022-09-13 03:08:49 +0000

    task 5 fi x

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const axios = require('axios').default;
-const url = process.argv[2];
-const file = require('fs');
-const nameFile = process.argv[2];
 
-axios.get(url)
-  .then(function (response) {
-    file.appendFile(nameFile, data, { encoding: 'utf8' },
-      function (error) {
-        if (error) { console.log(error); }
-      });
+const axios = require('axios');
+const fs = require('fs');
+
+axios
+  .get(process.argv[2])
+  .then(request => {
+    const text = request.data;
+    fs.appendFile(process.argv[3], text, { encoding: 'utf8' }, err => {
+      if (err) { console.log(err); }
+    });
   });
